77a82fea9fb66fc3dd81fff723470b0e169c0a03
PaoloForte95 <paolo.forte1995@gmail.com>
2022-12-15 12:20:53 +0200

    Fix.

--- orunav_launch/launch/single_wheel_loader.py
+++ orunav_launch/launch/single_wheel_loader.py
@@ -27,21 +27,19 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the launch directory
-
-    param_dir = get_package_share_directory('wheel_loader_launch')
+    wheel_loader_launch_dir = get_package_share_directory('wheel_loader_launch')
     wheel_loader_gazebo_dir = get_package_share_directory('wheel_loader_gazebo')
     description_dir = get_package_share_directory('wheel_loader_description')
-    wheel_loader_launch_dir = get_package_share_directory('wheel_loader_launch')
     maps_dir = get_package_share_directory('orunav_maps')
     worlds_dir = get_package_share_directory('gazebo_worlds_oru')
   
     # Create the launch configuration variables
     
 
-    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
     world = LaunchConfiguration('world')
     map_yaml_file = LaunchConfiguration('map')
+
     robots = [
         {'name': 'robot1', 'x_pose': '1.0', 'y_pose': '0.0', 'z_pose': '0.4',
                            'roll': '0.0', 'pitch': '0.0', 'yaw': '0.0'}]
@@ -62,13 +60,6 @@ def generate_launch_description():
         default_value=os.path.join(worlds_dir, 'worlds', 'empty.world'),
         description='Full path to world model file to load')
 
-    declare_bt_xml_cmd = DeclareLaunchArgument(
-        'default_bt_xml_filename',
-        default_value=os.path.join(
-            get_package_share_directory('nav2_bt_navigator'),
-            'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
-        description='Full path to the behavior tree xml file to use')
-
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
         default_value=os.path.join(
@@ -77,10 +68,9 @@ def generate_launch_description():
 
     declare_robot1_params_file_cmd = DeclareLaunchArgument(
         'robot1_params_file',
-        default_value=os.path.join(param_dir, 'params', 'L70_navigation_configs.yaml'),
+        default_value=os.path.join(wheel_loader_launch_dir, 'params', 'L70_navigation_configs.yaml'),
         description='Full path to the ROS2 parameters file to use for robot1 launched nodes')
 
-    machine_urdf=os.path.join(description_dir, 'urdf', 'L70.urdf')
         
     # Start Algoryx
     start_algoryx_cmd = ExecuteProcess(
@@ -97,10 +87,12 @@ def generate_launch_description():
                           'y_pose': robot['y_pose'],
                           'yaw': robot['yaw'],
                           'world':world,
-                          'use_gui': 'true',
+                          'use_gui': 'False',
                           }.items()))
 
-
+    default_nav_to_pose_bt_xml = os.path.join(
+            get_package_share_directory('plan2_behavior_tree'),
+            'behavior_trees', 'planning_w_navigation.xml')
 
     nav_instances_cmds = []
     for robot in robots:
@@ -121,8 +113,9 @@ def generate_launch_description():
                     launch_arguments={
                                     'namespace': robot['name'],
                                     'use_namespace': 'True',
+                                    'use_sim_time': 'True',
                                     'use_robot_state_pub': 'False',
-                                    'machine_xacro': machine_urdf,
+                                    'use_joint_state_publisher_gui': 'False',
                                     'rviz_config': rviz_config_file}.items()),
 
             
@@ -139,9 +132,11 @@ def generate_launch_description():
                            'rviz_config_file': rviz_config_file,
                            'map': map_yaml_file,
                            'params_file' : params_file,
-                           'default_bt_xml_filename': default_bt_xml_filename,
                            'use_robot_state_pub': 'False',
-                           'controller_prefix': 'controller/'
+                           'controller_prefix': 'controller/',
+                            'default_nav_to_pose_bt_xml': default_nav_to_pose_bt_xml,
+                           'use_composition': 'False',
+                           'use_selector': 'True'
                           }.items())
         
         ])
@@ -157,9 +152,10 @@ def generate_launch_description():
     ld.add_action(declare_robot1_params_file_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_world_cmd)
-    ld.add_action(declare_bt_xml_cmd)
     ld.add_action(declare_map_yaml_cmd)
-    #ld.add_action(start_algoryx_cmd)
+    
+    ld.add_action(start_algoryx_cmd)
+
     for spawn_robot_cmd in spawn_robots_cmds:
         ld.add_action(spawn_robot_cmd)
     for simulation_instance_cmd in nav_instances_cmds:
