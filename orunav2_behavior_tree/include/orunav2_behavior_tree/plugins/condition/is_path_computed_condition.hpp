7673716f471cefb06c2b069819714cd39ba6338d
PaoloForte95 <paolo.forte1995@gmail.com>
2023-01-20 10:34:14 +0200

    Added new conditions.

---
+++ orunav2_behavior_tree/include/orunav2_behavior_tree/plugins/condition/is_path_computed_condition.hpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2022 Paolo Forte
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ORUNAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_COMPUTED_CONDITION_HPP_
+#define ORUNAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_COMPUTED_CONDITION_HPP_
+
+#include <string>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "behaviortree_cpp_v3/condition_node.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/path.hpp"
+
+namespace orunav2_behavior_tree
+{
+
+/**
+ * @brief A BT::ConditionNode that returns SUCCESS when the path is computed and defined in the blackboard
+ * and FAILURE otherwise
+ */
+class IsPathComputedCondition : public BT::ConditionNode
+{
+public:
+  /**
+   * @brief A constructor for orunav2_behavior_tree::IsPathComputedCondition
+   * @param condition_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  IsPathComputedCondition(
+    const std::string & condition_name,
+    const BT::NodeConfiguration & conf);
+
+  IsPathComputedCondition() = delete;
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return {};
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  nav_msgs::msg::Path path_;
+  bool first_iteration_;
+};
+
+}  // namespace orunav2_behavior_tree
+
+#endif  // ORUNAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_COMPUTED_CONDITION_HPP_
---
+++ orunav2_behavior_tree/plugins/condition/is_planner_equal_condition.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2022 Paolo Forte
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "orunav2_behavior_tree/plugins/condition/is_planner_equal_condition.hpp"
+#include <chrono>
+#include <memory>
+#include <string>
+
+namespace orunav2_behavior_tree
+{
+
+IsPlannerEqualCondition::IsPlannerEqualCondition(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf)
+{
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+
+  getInput<std::string>("planner_to_check", planner_id_);
+  first_iteration_ = true;
+ 
+}
+
+BT::NodeStatus IsPlannerEqualCondition::tick()
+{
+  if(!first_iteration_){
+    config().blackboard->get<std::string>("planner", actual_planner_);
+  }
+  if (planner_id_ != actual_planner_ )
+  {
+    first_iteration_ = false;
+    return BT::NodeStatus::FAILURE;
+  }
+  return BT::NodeStatus::SUCCESS;
+  
+}
+
+}  // namespace orunav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<orunav2_behavior_tree::IsPlannerEqualCondition>("IsPlannerEqualCondition");
+}
